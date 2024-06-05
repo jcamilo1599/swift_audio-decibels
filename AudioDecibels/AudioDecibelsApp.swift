fd4e05d8643211526223612deac6154009318c3d
Juan Camilo <99774186+jcamilo1599@users.noreply.github.com>
2024-06-05 01:02:01 -0500

    Initial Commit

---
+++ AudioDecibels/AudioDecibelsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioDecibelsApp.swift
+//  AudioDecibels
+//
+//  Created by Juan Camilo Marín Ochoa on 5/06/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioDecibelsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioDecibels/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AudioDecibels
+//
+//  Created by Juan Camilo Marín Ochoa on 5/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
