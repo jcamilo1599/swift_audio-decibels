a0139f59780e382e51d2ce0f05578d2acc66b2ad
Juan Camilo Marin Ochoa <jmarin1599@icloud.com>
2024-07-21 23:21:06 -0500

    Se crea aplicación para watchOS

---
+++ AudioDecibelsWatch/AudioDecibelsWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioDecibelsWatchApp.swift
+//  AudioDecibelsWatchApp
+//
+//  Created by Juan Camilo Marin Ochoa on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct AudioDecibelsWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioDecibelsWatch/ContentView.swift
@@ -0,0 +1,87 @@
+//
+//  ContentView.swift
+//  AudioDecibelsWatch
+//
+//  Created by Juan Camilo Marin Ochoa on 21/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @StateObject private var audioManager = AudioManager()
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                Text("\(audioManager.decibels, specifier: "%.1f") dB")
+                    .font(.title)
+                    .foregroundStyle(.white)
+                    .padding()
+                    .frame(width: 200)
+                    .cornerRadius(16)
+                
+                Divider()
+                
+                HStack {
+                    Button(action: {
+                        audioManager.startMonitoring()
+                    }) {
+                        VStack {
+                            Image(systemName: "play.circle")
+                                .font(.title)
+                                .foregroundColor(.green)
+                                .padding(.bottom, 3)
+                            
+                            Text("Iniciar")
+                        }
+                    }
+                    
+                    Button(action: {
+                        audioManager.stopMonitoring()
+                    }) {
+                        VStack {
+                            Image(systemName: "stop.circle")
+                                .font(.title)
+                                .foregroundColor(.red)
+                                .padding(.bottom, 3)
+                            
+                            Text("Detener")
+                        }
+                    }
+                }
+                .padding()
+                
+                Divider()
+                
+                VStack {
+                    HStack {
+                        Spacer()
+                        Text("AVG")
+                        Text("\(audioManager.avgDecibels, specifier: "%.1f") dB")
+                            .bold()
+                    }
+                    
+                    HStack {
+                        Spacer()
+                        Text("Mínimo")
+                        Text("\(audioManager.minimum ?? 0, specifier: "%.1f") dB")
+                            .bold()
+                    }
+                    
+                    HStack {
+                        Spacer()
+                        Text("Máximo")
+                        Text("\(audioManager.maximum ?? 0, specifier: "%.1f") dB")
+                            .bold()
+                    }
+                }
+                .padding()
+                .navigationTitle("Monitor de Decibeles")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
